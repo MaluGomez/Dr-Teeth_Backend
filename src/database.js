d1462dad9c5d90b2cf96dbf1c664a1db7fd3beb3
EulyGV <euly.garces.2016@upb.edu.co>
2020-12-15 11:38:09 -0500

    cambios proyecto

--- src/database.js
+++ src/database.js
@@ -2,12 +2,12 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'drteeth.mysql.database.azure.com',
-  user: 'drteeth@drteeth',
-  password: 'teethdr99.',
-  database: 'drteeth',
-  ssl: true,
-  multipleStatements: true
+  host: process.env.DB_HOST ||'drteeth.mysql.database.azure.com',
+  user: process.env.DB_USER || 'drteeth@drteeth',
+  password: process.env.DB_PASSWORD ||'teethdr99.',
+  database: process.env.DB_DATABASE ||'drteeth',
+  ssl: process.env.DB_SSL || true,
+  multipleStatements: process.env.DB_MULTIPLESTATEMENTS || true
 });
 
 mysqlConnection.connect(function (err) {
