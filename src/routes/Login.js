96800b3120903bb378bf323c3c3669de9a0feac8
EulyGV <euly.garces.2016@upb.edu.co>
2021-01-21 17:26:20 -0500

    Actualizando
    Listar odontólogo y administrador

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,21 +3,27 @@ const router = express.Router();
 
 const mysqlConnection = require("../database");
 
-router.post("/Auth", (req, res) => {
+  router.post("/Auth", (req, res) => {
     let pass = req.body.password
     let userEmail = req.body.user
-    console.log(req.body, ' -- ', userEmail, ' -- ', pass)
     mysqlConnection.query(
       "SELECT * FROM proyecto_drteeth.Administrador WHERE email = ? AND contrasena = ?;",[userEmail, pass],
       (err, rows, fields) => {
         if (!err) {
+          if (rows.length == 0) {
+            mysqlConnection.query(
+              "SELECT * FROM proyecto_drteeth.Odontologo WHERE email = ? AND contrasena = ?;",[userEmail, pass],
+              (err, rows, fields) => {
+                res.json(rows.length > 0 ? rows : { mesagge: "Usuario o contraseña incorrecta! o no existe usuario!" });
+              })
+          } else {
             res.json(rows.length > 0 ? rows : { mesagge: "Usuario o contraseña incorrecta!" });
+          }
         } else {
           console.log(err);
         }
       }
     );
   });
-
 
   module.exports = router;
